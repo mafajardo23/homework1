599ed3e390472770b0df6e29991e45e214c64beb
mafajardo23 <mafajardo@davidson.edu>
2026-09-13 11:17:38 -0400

    Start

---
+++ PuzzleBoard.java
@@ -0,0 +1,17 @@
+
+/**
+ * Class: CSC-370
+ *
+ * @author: Maria Fajardo & Jack Bray
+ * 
+ * Purpose: Implement A* algorithm + Reproduce the
+ * experiment of Russell and Norvig in the 8-puzzle domain.
+ *
+ */
+
+public class PuzzleBoard {
+    int [][] board;
+    int[] blankPos = new int[2];  
+
+
+}
