814d48e732c8d2c6ff1e5436412b9d3999e3d215
mafajardo23 <mafajardo@davidson.edu>
2026-09-23 20:27:43 -0400

    row check

--- PuzzleBoard.java
+++ PuzzleBoard.java
@@ -144,23 +144,76 @@ public class PuzzleBoard {
             List<Integer> conflictCandidates = new ArrayList<>();
 
             for (int col = 0; col < 3; col++) {
-                int currPosition = row * 3 + col;
-                int currNumber = board[currPosition];
+                int position = row * 3 + col;
+                int tile = board[position];
 
-                if (currNumber != 0) { //ignore blank
-                    int goalRow = currNumber / 3;
+                if (tile != 0) { //ignore blank
+                    int goalRow = tile / 3;
                     if (goalRow == row) {
-                        conflictCandidates.add(currPosition);
+                        conflictCandidates.add(position);
+                    }
+                }
+            }
+
+            for (int remRound = 0; remRound < conflictCandidates.size(); remRound++) {
+                int conflictFound = 0;
+
+                for (int currIndex = 0; currIndex < conflictCandidates.size(); currIndex++) {
+                    int currPosition = conflictCandidates.get(currIndex);
+                    int currNumber = board[currPosition];
+                    int currCol = currPosition % 3;
+                    int currGoalCol = currNumber % 3;
+
+                    for (int otherIndex = currIndex + 1; otherIndex < conflictCandidates.size(); otherIndex++) {
+                        int otherPosition = conflictCandidates.get(otherIndex);
+                        int otherNumber = board[otherPosition];
+                        int otherCol = otherPosition % 3;
+                        int otherGoalCol = otherNumber % 3;
+
+                        if (currCol < otherCol && currGoalCol > otherGoalCol) {
+                            conflictFound++;
+                        }
                     }
                 }
 
-                for (int remRound = 0; remRound < conflictCandidates.size(); remRound++) {
-                    
+                if (conflictFound == 0) {
+                    remRound = conflictCandidates.size();
+                } else {
+                    int highestConflictIndex = 0;
+                    int highestConflictCount = 0;
+
+                    for (int currIndex = 0; currIndex < conflictCandidates.size(); currIndex++) {
+
+                        int currPosition = conflictCandidates.get(currIndex);
+                        int currNumber = board[currPosition];
+                        int currCol = currPosition % 3;
+                        int currGoalCol = currNumber % 3;
+
+                        int count = 0;
+                        for (int otherIndex = 0; otherIndex < conflictCandidates.size(); otherIndex++) {
+                            if (currIndex != otherIndex) {
+                                int otherPosition = conflictCandidates.get(otherIndex);
+                                int otherNumber = board[otherPosition];
+                                int otherCol = otherPosition % 3;
+                                int otherGoalCol = otherNumber % 3;
+
+                                if (currCol < otherCol && currGoalCol > otherGoalCol) {
+                                    count++;
+                                }
+                            }
+                        }
+
+                        if (count > highestConflictCount) {
+                            highestConflictCount = count;
+                            highestConflictIndex = currIndex;
+                        }
+                    }
+
+                    conflictCandidates.remove(highestConflictIndex);
+                    total += 2;
                 }
             }
         }
-
-
 
         return total;
     }
