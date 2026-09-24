77ac824296c2969cfa2b440632e56621a45d868a
jackbray287 <jabray@davidson.edu>
2026-09-23 20:41:59 -0400

    Add fourth heuristic and update calculations

--- Experiment.java
+++ Experiment.java
@@ -24,9 +24,11 @@ public class Experiment {
             double h1TotalNodes = 0;
             double h2TotalNodes = 0;
             double h3TotalNodes = 0;
+            double h4TotalNodes = 0;
             double bFactorh1 = 0;
             double bFactorh2 = 0;
-            double bFactorh3 = 0;   
+            double bFactorh3 = 0;
+            double bFactorh4 = 0;
             int n = boards.size();
 
             for (int[] board : boards) {
@@ -34,14 +36,17 @@ public class Experiment {
                 Search.SearchResult result1 = Search.astar(board, Search.MISPLACED_TILES);
                 Search.SearchResult result2 = Search.astar(board, Search.MANHATTAN);
                 Search.SearchResult result3 = Search.astar(board, Search.LINEAR_CONFLICT);
+                Search.SearchResult result4 = Search.astar(board, Search.ZERO);
 
                 h1TotalNodes += result1.nodesGenerated;
                 h2TotalNodes += result2.nodesGenerated;
                 h3TotalNodes += result3.nodesGenerated;
+                h4TotalNodes += result4.nodesGenerated;
 
                 bFactorh1 += PuzzleBoard.bFactor(result1.nodesGenerated, result1.depth);
                 bFactorh2 += PuzzleBoard.bFactor(result2.nodesGenerated, result2.depth);
                 bFactorh3 += PuzzleBoard.bFactor(result3.nodesGenerated, result3.depth);
+                bFactorh4 += PuzzleBoard.bFactor(result4.nodesGenerated, result4.depth);
 
             }
 
@@ -49,9 +54,11 @@ public class Experiment {
             System.out.println("h1 nodes = " + (h1TotalNodes / n));
             System.out.println("h2 nodes = " + (h2TotalNodes / n));
             System.out.println("h3 nodes = " + (h3TotalNodes / n));
+            System.out.println("h4 nodes = " + (h4TotalNodes / n));
             System.out.println("h1 b* = " + (bFactorh1 / n));
             System.out.println("h2 b* = " + (bFactorh2 / n));
             System.out.println("h3 b* = " + (bFactorh3 / n));
+            System.out.println("h4 b* = " + (bFactorh4 / n));
         }
     }
 }
